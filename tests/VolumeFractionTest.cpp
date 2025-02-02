24d3a0c8aefebe46be72e75fc70ea4129e546a21
Jacob Jones <darkstorm143@gmail.com>
2025-02-02 15:23:04 -0700

    anotha one

--- tests/VolumeFractionTest.cpp
+++ tests/VolumeFractionTest.cpp
@@ -86,3 +86,49 @@ TEST(VolumeFractionTest, ComputeVolumeFractionsFlower){
     ASSERT_TRUE(percent_error < 0.05);
 
 }
+
+TEST(VolumeFractionTest, ComputeVolumeFractionsCardioid){
+
+    // Define the number of segments
+    const int num_segments = 1000;
+    const double pi = 3.14159265358979323846;
+    const double a = 0.1;
+
+    // Create coordinates for the unit circle
+    coords coordinates;
+    for (int i = 0; i < num_segments; ++i) {
+        double t = 2 * pi * i / num_segments;
+        double x = 2*a*(1-cos(t))*cos(t)/2+ 0.5;
+        double y = 2*a*(1-cos(t))*sin(t)/2+ 0.5;
+        coordinates.push_back({x,y});
+    }
+
+    // Create segment ids for the unit circle
+    segment_ids segments;
+    for (int i = 0; i < num_segments; ++i) {
+        segments.push_back({i, (i + 1) % num_segments});
+    }
+
+    // Create the IntervalTree
+    IntervalTree<Axis::Y> tree(segments, coordinates);
+
+    double exact = 0.015*M_PI;
+
+    BBox box{-0.01, 1.01, -0.01, 1.01};
+    Grid grid(box, 50, 50);
+    grid.AddShape(tree);
+    grid.ComputeVolumeFractions(20);
+    double total_volume = grid.ComputeTotalVolume();
+    double percent_error = 100 * fabs(total_volume - exact) / exact;
+    std::cout << "percent_error" << percent_error << std::endl;
+    ASSERT_TRUE(percent_error < 5);
+
+    Grid grid2(box, 500, 500);
+    grid2.AddShape(tree);
+    grid2.ComputeVolumeFractions(100);
+    total_volume = grid2.ComputeTotalVolume();
+    percent_error = 100 * fabs(total_volume - exact) / exact;
+    std::cout << "percent_error" << percent_error << std::endl;
+    ASSERT_TRUE(percent_error < 0.1);
+
+}
