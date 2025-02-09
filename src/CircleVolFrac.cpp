99e83d19c113e043834b534cd41b5369d031a297
Jacob Jones <darkstorm143@gmail.com>
2025-02-09 08:24:23 -0700

    fix ci

--- src/CircleVolFrac.cpp
+++ src/CircleVolFrac.cpp
@@ -2,20 +2,20 @@
 #include <assert.h>
 
 
-// ...existing code...
-__attribute__((always_inline)) static inline double section(double h, double r = 1) {
+
+static inline double section(double h, double r = 1) {
     // Use builtin_expect to help branch prediction
     if (__builtin_expect(h < r, 1))
         return sqrt(r * r - h * h);
     return 0.0;
 }
-// ...existing code...
-__attribute__((always_inline)) static inline double g(double x, double h, double r = 1) {
+
+static inline double g(double x, double h, double r = 1) {
     double rr = r * r;
     return 0.5 * (sqrt(1.0 - (x * x) / rr) * x * r + rr * asin(x / r) - 2.0 * h * x);
 }
-// ...existing code...
-__attribute__((always_inline)) static inline double area(double x0, double x1, double h, double r) {
+
+static inline double area(double x0, double x1, double h, double r) {
     if (__builtin_expect(x0 > x1, 0))
         std::swap(x0, x1);
     double s = section(h, r);
@@ -23,9 +23,8 @@ __attribute__((always_inline)) static inline double area(double x0, double x1, d
     double right = std::max(-s, std::min(s, x1));
     return g(right, h, r) - g(left, h, r);
 }
-// ...existing code...
 
-__attribute__((always_inline)) static inline double area(double x0, double x1, double y0, double y1, double r)
+static inline double area(double x0, double x1, double y0, double y1, double r)
 {
     // Help the compiler predict branches
     if (__builtin_expect(y0 > y1, 0))
